4b37784b83b397ee6bd8549f6bc9ac05bfb38ff0
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-06-03 15:43:47 -0300

    Configuração inicial

---
+++ src/main/java/br/ufrn/imd/utravel/UtravelApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.utravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtravelApplication.class, args);
+    }
+
+}
