e45c55dc8af2d993f703ab954bd26f9003542ca4
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-06-16 19:26:49 -0300

    Arruma erro no service e controller de empresa

--- src/main/java/br/ufrn/imd/utravel/repository/EmpresaRepository.java
+++ src/main/java/br/ufrn/imd/utravel/repository/EmpresaRepository.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
