053608009190906a16f3a742b2e7accae312ba19
Rayane <mrayalves05@gmail.com>
2019-06-17 14:02:06 -0300

    Nada

--- src/main/java/br/ufrn/imd/utravel/repository/ReservaRepository.java
+++ src/main/java/br/ufrn/imd/utravel/repository/ReservaRepository.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-import java.sql.Time;
 import java.util.List;
 import java.util.Optional;
 
